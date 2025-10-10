0b98a15f94552d9c59c44cadf317fefe3ef733d0
Jeean1 <giraldojeanpool@hotmail.com>
2025-10-10 12:45:13 -0500

    vercel deploy v2

--- src/components/services/config/host.js
+++ src/components/services/config/host.js
@@ -1,8 +1,7 @@
 //esto debe ser un .env - local pongo esto, en prod coloco el .env de producción
-// export const apiHost = "http://localhost:4000";
+// export const apiHost = "http://localhost:4000"; //Apunta a api local, pero api local trabaja con datos de produccción.
 
 export const apiHost = "https://roux-sky.up.railway.app"
-
 
 export const authAPI = "/auth";
 export const privateAPI = "/private/api";
@@ -10,3 +9,4 @@ export const publicAPI = "/public/api";
 export const ordersAPI = "/orders";
 export const quickOrdersApi = "/quick-orders";
 export const onlineUsersAPI = "/user-connections";
+export const locationsAPI = "/locations";
